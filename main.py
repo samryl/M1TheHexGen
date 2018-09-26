3077aa6614824b94bcfaedb2c63e9cea11bb5b9b
Samusoidal <info@samusoidal.com>
2018-09-26 07:51:53 -0700

    MORE

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import sys, itertools, math
 from collections import OrderedDict
 from tkinter import *
 from tkinter import filedialog
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 def load_db():
     dbfile = open("translation.ini").read()
@@ -16,10 +19,12 @@ def load_db():
 
 def translate_letter_to_char(_let):
     global db
+    global dbe
     return list(db.keys())[list(db.values()).index(_let)]
 
 def translate_letter_to_hex(_let):
     global db
+    global dbe
     return db[_let]
 
 def translate_string_to_char(_str):
@@ -38,7 +43,7 @@ def translate_string_to_char(_str):
 def translate_string_to_hex(_str):
 
     _newstring = ""
-    _str = _str.upper()
+    _str = _str#.upper()
 
     i = 0
     while i < len(_str):
@@ -59,6 +64,12 @@ def get_line_length():
         return 15
     elif e == 'Title: Press Play':
         return 21
+    elif e == 'Title: Copyright':
+        return 18
+    elif e == 'Ending: Header':
+        return 8
+    elif e == 'Ending: Body':
+        return 21
 
 def get_max_lines():
     e = s_editing.get()
@@ -72,6 +83,29 @@ def get_max_lines():
         return 1
     elif e == 'Title: Press Play':
         return 1
+    elif e == 'Title: Copyright':
+        return 1
+    elif e == 'Ending: Header':
+        return 1
+    elif e == 'Ending: Body':
+        return 6
+
+def get_ending_line_len(ln):
+    print(ln)
+    if ln == 1:
+        return 19
+    elif ln == 2:
+        return 21
+    elif ln == 3:
+        return 18
+    elif ln == 4:
+        return 11
+    elif ln == 5:
+        return 18
+    elif ln == 6:
+        return 6
+    else:
+        return 1
 
 def parse_char():
 
@@ -84,8 +118,15 @@ def parse_char():
     ln = 1
     for line in _toparse.splitlines():
         if len(line) > 0:
-            _parsed += line[:_line_len]
+
+            if s_editing.get() == 'Ending: Body':
+                _parsed += line[:get_ending_line_len(ln)]
+
+            else:
+                _parsed += line[:_line_len]
+
             ln += 1
+
         if ln > _max_lines:
             break
         else:
@@ -97,7 +138,6 @@ def parse_char():
     while len(_parsed.splitlines()) > _max_lines:
         _parsed = _parsed[:-1]
 
-    print(len(_parsed.splitlines()))
     input_char.delete("1.0",END)
     input_char.insert("1.0",_parsed)
 
@@ -112,6 +152,8 @@ def btn_tohex_clicked():
     ln = 1
     _justification = s_centered.get()
     for line in _toconv.splitlines():
+        if s_editing.get() == 'Ending: Body':
+            _line_len = get_ending_line_len(ln)
         _hexline = translate_string_to_hex(line[:_line_len])
         if len(line) < _line_len:
             if _justification != "centered": # left or right justification
@@ -134,6 +176,7 @@ def btn_tohex_clicked():
                 _hexline += "FF FF "
 
         _convertedhex += _hexline + "\n"
+
     input_hex.delete("1.0",END)
     input_hex.insert("1.0",_convertedhex)
 
@@ -144,7 +187,7 @@ def btn_tochar_clicked():
 
 def getROM():
     global ROM
-    romfile = filedialog.askopenfilename(initialdir = "/",title = "Select ROM")
+    romfile = filedialog.askopenfilename(initialdir = dir_path,title = "Select ROM")
     e_filepath.config(state='normal')
     e_filepath.delete("1.0",END)
     e_filepath.insert("1.0",romfile)
@@ -177,6 +220,17 @@ def patchROM():
             writeHexString(f, "0x00067B", t[0])
         elif e == 'Title: Press Play':
             writeHexString(f, "0x000513", t[0])
+        elif e == 'Title: Copyright':
+            writeHexString(f, "0x00052B", t[0])
+        elif e == 'Ending: Header':
+            writeHexString(f, "0x0021D5", t[0])
+        elif e == 'Ending: Body':
+            writeHexString(f, "0x0021E0", t[0])
+            writeHexString(f, "0x0021FE", t[1])
+            writeHexString(f, "0x002122", t[2])
+            writeHexString(f, "0x00213D", t[3])
+            writeHexString(f, "0x002153", t[4])
+            writeHexString(f, "0x00216E", t[5])
 
         f.close()
 
@@ -190,7 +244,7 @@ root.configure(padx=12, pady=12)
 f_char = Frame(root)
 
 s_editing = StringVar(root)
-texts = {'Intro: Header', 'Intro: Body', 'Intro: Signature', 'Intro: Signature 2', 'Title: Press Play', 'Ending: Header', 'Ending: Body'}
+texts = {'Intro: Header', 'Intro: Body', 'Intro: Signature', 'Intro: Signature 2', 'Title: Press Play', 'Title: Copyright', 'Ending: Header', 'Ending: Body'}
 s_editing.set('Intro: Body')
 
 l_char = OptionMenu(f_char, s_editing, *sorted(texts))
